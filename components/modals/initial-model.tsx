296528eb9f256cebd116ac437dc425c92c53ac14
ogberagabriel234@gmail.com <ogberagabriel234@gmail.com>
2023-09-24 22:59:44 +0100

    Added Menu for Server Page

--- components/modals/initial-model.tsx
+++ components/modals/initial-model.tsx
@@ -23,6 +23,8 @@ import  * as z from 'zod';
 import {  zodResolver } from '@hookform/resolvers/zod'
 import { useEffect, useState } from 'react';
 import { FileUpload } from '@/components/file-upload';
+import axios from 'axios'
+import { useRouter } from 'next/navigation';
 
 
 const formSchema = z.object({
@@ -37,6 +39,8 @@ const formSchema = z.object({
 
 export const InitialModal = () => {
     const [isMounted, setIsMouted] = useState(false);
+
+    const router = useRouter();
 
     useEffect(() => {
      setIsMouted(true)
@@ -54,7 +58,16 @@ export const InitialModal = () => {
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values)
+    try {
+     await axios.post('/api/servers', values)
+
+     form.reset()
+     router.refresh()
+     window.location.reload()
+
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   if (!isMounted) {
